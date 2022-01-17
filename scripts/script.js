381fc8fbe6af88ccd607c93fc3aa9efe3378612f
Viktor Markov <markov.viktor.an@yandex.ru>
2022-01-17 16:50:30 +0300

    finish popup

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,28 @@ popup.addEventListener('click', function(event) {
     closePopup()
   }
 })
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');  // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__user-name');  // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__user-job');// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+let userName = nameInput.value;    
+let userJob = jobInput.value;
+// Получите значение полей jobInput и nameInput из свойства value
+let profileName = document.querySelector('.profile__user-name');
+let profileJob = document.querySelector('.profile__user-job');
+    // Выберите элементы, куда должны быть вставлены значения полей
+profileName.textContent = userName;
+profileJob.textContent = userJob;
+closePopup()
+    // Вставьте новые значения с помощью textContent
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
