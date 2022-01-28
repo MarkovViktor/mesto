9bd8fdcbbd315576ce8f89fd7cb7ec3136f19373
Viktor Markov <markov.viktor.an@yandex.ru>
2022-01-28 19:08:14 +0300

    make_trash_basket

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ function closePopup() {
 }
 // Обработчик «отправки» формы.
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    evt.preventDefault();
 // Выберем элементы, куда должны быть вставлены значения полей
 profileName.textContent = nameInput.value;
 profileJob.textContent = jobInput.value;
@@ -30,10 +30,52 @@ closePopup()
 formElement.addEventListener('submit', formSubmitHandler);
 profileOpenPopupButton.addEventListener('click', openPopup)
 popupCloseButton.addEventListener('click', closePopup)
-
 //Закрытие попапа кликом на пустую область.
 //popup.addEventListener('click', function(event) {
 // if(event.target === event.currentTarget){
 //closePopup()
 //  }
 //})
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const card = ({name, link}) => {
+  const placeCard = document.createElement("div");
+  placeCard.classList.add('place');
+  placeCard.insertAdjacentHTML(
+    'afterBegin',
+    `
+    <div class="place">
+      <img class="place__image" src=${link} alt=${name}>
+      <h2 class="place__title">${name}</h2>
+      <button class="place__like" type="button" aria-label="поставить лайк"></button>
+      <button class="place__delete" type="button" aria-label="удалить"></button>
+    </div>
+ `
+  );
+  return placeCard;
+};
